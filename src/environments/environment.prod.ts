fea7a8cc08d8719f25ff715d20afb85f9d6252bd
Isidro Moreno <imoreno.itservices@gmail.com>
2023-07-28 11:19:25 -0300

    chore: install Storybook

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  web3FormsApiKey: '922b18af-f56a-436b-afd2-7986d6232add',
+  web3FormsUrl: 'https://api.web3forms.com/submit'
 };
